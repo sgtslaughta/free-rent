a4b5304bc29952aa415b2361c669be2ea4aa1ca5
sgtslaughta <richardsoto1010@gmail.com>
2024-03-23 16:53:47 -0400

    Added tenant page

--- python/src_lib/dashboard/tenant.py
+++ python/src_lib/dashboard/tenant.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from .tenant_funcs.alter_source import add_tenant
+from .tenant_funcs.alter_source import add_tenant, edit_tenant
 
 
 def get_tenants():
@@ -11,6 +11,8 @@ def get_tenants():
 def show(container):
     if "adding_tenant" not in st.session_state:
         st.session_state.adding_tenant = False
+    if "edit_tenant" not in st.session_state:
+        st.session_state.edit_tenant = False
     if "deleting_tenant" not in st.session_state:
         st.session_state.deleting_tenant = False
     container.button("Refresh Data", key="refresh_tenants")
@@ -23,11 +25,13 @@ def show(container):
 
 
 def show_all_tenants(container, tenants):
-    headers = tenants.columns.tolist()
-    headers.remove("id")
     container.metric("Count", len(tenants))
-    container.markdown("*Click in table then CTL-F to search*")
-    container.data_editor(tenants[headers], hide_index=True, key="tenants")
-    add_t, del_t, refresh_t = container.columns(3)
-    if add_t.button("Add") or st.session_state.adding_tenant:
+    container.markdown("*Hover over table then click magnifying glass to search*")
+    container.data_editor(tenants, hide_index=True, key="tenants")
+    add_b, edit_b, del_b = container.columns(3)
+    if add_b.button("Add", use_container_width=True) or st.session_state.adding_tenant:
         add_tenant(container)
+    if edit_b.button("Edit Tenant", use_container_width=True) or st.session_state.edit_tenant:
+        edit_tenant(container, tenants)
+    if del_b.button("Delete Selected", use_container_width=True):
+        pass
